41c1b4f163d8d20192d8892eb2e5e156629f2abb
Sebastian Kaluzinski <skaluzinski1801@gmail.com>
2024-12-02 00:09:28 +0100

    feat: implemented business logic associated with order creation

--- src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/OrderRepository.java
+++ src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/OrderRepository.java
@@ -1,9 +1,12 @@
 package pl.edu.pw.ee.catering_backend.infrastructure.db.repositories;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.edu.pw.ee.catering_backend.infrastructure.db.Order;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-
+    @Query("SELECT o FROM orders o WHERE o.client.login = :clientLogin")
+    List<Order> findByClientLogin(String clientLogin);
 }
---
+++ src/main/java/pl/edu/pw/ee/catering_backend/orders/domain/CreateOrderUseCase.kt
@@ -0,0 +1,44 @@
+package pl.edu.pw.ee.catering_backend.orders.domain
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import pl.edu.pw.ee.catering_backend.infrastructure.db.repositories.ClientRepository
+import pl.edu.pw.ee.catering_backend.infrastructure.db.repositories.MealRepository
+import pl.edu.pw.ee.catering_backend.orders.data.OrdersService
+import java.util.UUID
+
+@Service
+class CreateOrderUseCase(
+    @Autowired private val ordersService: OrdersService,
+    @Autowired private val mealsRepository: MealRepository,
+    @Autowired private val clientRepository: ClientRepository
+) {
+    operator fun invoke(
+        clientLogin: String,
+        mealsIds: List<String>,
+        deliveryAddress: String,
+        deliveryMethod: String,
+        status: String
+    ) : Result<String> {
+        val mealsUUIDs = mealsIds.map { UUID.fromString(it) }
+        val meals = mealsRepository.findAllById(mealsUUIDs)
+        val client = clientRepository.findByLogin(clientLogin) ?: return Result.failure(Exception("Client not found"))
+
+        if (meals.size != mealsIds.size) {
+            return Result.failure(Exception("Some meals not found"))
+        }
+
+        if (meals.any { it.available == false }) {
+            return Result.failure(Exception("Some meals are not available"))
+        }
+
+        if (client.size != 1) {
+            return Result.failure(Exception("Client not found"))
+        }
+
+
+
+        return ordersService.createOrder(client.first(), meals, deliveryAddress, deliveryMethod, status).map { it.id.toString() }
+
+    }
+}
