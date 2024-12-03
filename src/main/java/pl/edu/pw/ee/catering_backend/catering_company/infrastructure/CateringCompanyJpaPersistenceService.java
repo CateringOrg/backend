0208720e88f94836f41f1e2f9766d8c1a984e545
b9dn <mabogdan35@gmail.com>
2024-12-03 22:44:21 +0100

    Remove unnecessary db mapper

--- src/main/java/pl/edu/pw/ee/catering_backend/catering_company/infrastructure/CateringCompanyJpaPersistenceService.java
+++ src/main/java/pl/edu/pw/ee/catering_backend/catering_company/infrastructure/CateringCompanyJpaPersistenceService.java
@@ -2,12 +2,9 @@ package pl.edu.pw.ee.catering_backend.catering_company.infrastructure;
 
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants.ComponentModel;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.ee.catering_backend.catering_company.comms.CateringCompanyMapper;
 import pl.edu.pw.ee.catering_backend.catering_company.domain.ICateringCompanyPersistenceService;
-import pl.edu.pw.ee.catering_backend.infrastructure.db.CateringCompany;
 import pl.edu.pw.ee.catering_backend.infrastructure.db.repositories.CateringCompanyRepository;
 
 @RequiredArgsConstructor
@@ -15,30 +12,18 @@ import pl.edu.pw.ee.catering_backend.infrastructure.db.repositories.CateringComp
 class CateringCompanyJpaPersistenceService implements ICateringCompanyPersistenceService {
 
   private final CateringCompanyRepository cateringCompanyRepository;
-  private final CateringCompanyDbMapper cateringCompanyDbMapper;
+  private final CateringCompanyMapper cateringCompanyMapper;
 
   @Override
   public pl.edu.pw.ee.catering_backend.catering_company.domain.CateringCompany save(
       pl.edu.pw.ee.catering_backend.catering_company.domain.CateringCompany cateringCompany) {
-    return cateringCompanyDbMapper.mapToDomain(
-        cateringCompanyRepository.save(cateringCompanyDbMapper.mapToDb(cateringCompany)));
+    return cateringCompanyMapper.mapToDomain(
+        cateringCompanyRepository.save(cateringCompanyMapper.mapToDb(cateringCompany)));
   }
 
   @Override
   public pl.edu.pw.ee.catering_backend.catering_company.domain.CateringCompany get(UUID id) {
-    return cateringCompanyDbMapper.mapToDomain(
+    return cateringCompanyMapper.mapToDomain(
         cateringCompanyRepository.findById(id).orElseThrow());
-  }
-
-  @Mapper(componentModel = ComponentModel.SPRING)
-  interface CateringCompanyDbMapper {
-
-    @Mapping(target = "NIP", source = "nip")
-    pl.edu.pw.ee.catering_backend.catering_company.domain.CateringCompany mapToDomain(
-        CateringCompany cateringCompany);
-
-    @Mapping(target = "nip", source = "NIP")
-    CateringCompany mapToDb(
-        pl.edu.pw.ee.catering_backend.catering_company.domain.CateringCompany cateringCompany);
   }
 }
