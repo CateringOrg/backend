863627e939da90b3e86af386063545bfd2eb6703
skaluzinski <57567676+skaluzinski@users.noreply.github.com>
2024-12-05 21:48:17 +0100

    Revert "feat: implemented business logic associated with order creation"

--- src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/ClientRepository.java
+++ src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/ClientRepository.java
@@ -3,9 +3,6 @@ package pl.edu.pw.ee.catering_backend.infrastructure.db.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.pw.ee.catering_backend.infrastructure.db.Client;
 
-import java.util.List;
-
 public interface ClientRepository extends JpaRepository<Client, String> {
 
-    List<Client> findByLogin(String login);
 }
