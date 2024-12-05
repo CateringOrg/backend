35bb623eceafdd41f631c62b43f4487c0404ff38
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2024-12-05 14:36:40 +0100

    Add files via upload

--- src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/CateringCompanyRepository.java
+++ src/main/java/pl/edu/pw/ee/catering_backend/infrastructure/db/repositories/CateringCompanyRepository.java
@@ -1,9 +1,16 @@
 package pl.edu.pw.ee.catering_backend.infrastructure.db.repositories;
 
+import jakarta.transaction.Transactional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.edu.pw.ee.catering_backend.infrastructure.db.CateringCompany;
 
 public interface CateringCompanyRepository extends JpaRepository<CateringCompany, UUID> {
 
+  @Query("insert into catering_companies (id, name, nip, address) values (:id, :name, :nip, :address) on conflict (id) do update set name = :name, nip = :nip, address = :address")
+  @Modifying
+  @Transactional
+  void saveManually(UUID id, String name, String nip, String address);
 }
