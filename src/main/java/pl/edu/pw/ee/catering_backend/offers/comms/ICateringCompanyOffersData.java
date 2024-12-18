b392dd196ea5ed2df24727b77a0484d3726703f5
b9dn <mabogdan35@gmail.com>
2024-12-18 14:24:00 +0100

    Remove unnecessary

--- src/main/java/pl/edu/pw/ee/catering_backend/offers/comms/ICateringCompanyOffersData.java
+++ src/main/java/pl/edu/pw/ee/catering_backend/offers/comms/ICateringCompanyOffersData.java
@@ -5,12 +5,10 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.multipart.MultipartFile;
-import pl.edu.pw.ee.catering_backend.offers.comms.dtos.AddMealDTO;
 import pl.edu.pw.ee.catering_backend.offers.comms.dtos.AddMealWithPhotosDTO;
 import pl.edu.pw.ee.catering_backend.offers.comms.dtos.GetMealDTO;
 
 public interface ICateringCompanyOffersData {
-  ResponseEntity<Void> addMeal(UUID cateringCompanyId, AddMealWithPhotosDTO addMealWithPhotosDTO);//, AddMealWithPhotosDTO addMealDTO);
+  ResponseEntity<Void> addMeal(UUID cateringCompanyId, AddMealWithPhotosDTO addMealWithPhotosDTO);
   List<GetMealDTO> getMealsByCompany(@PathVariable UUID cateringCompanyId);
 }
